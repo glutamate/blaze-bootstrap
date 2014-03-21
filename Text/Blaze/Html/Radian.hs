aabc61d7a8765030d77efb516ae14e011e881798
Tom Nielsen <tanielsen@gmail.com>
2014-03-21 10:46:03 +0000

    begin Radian

--- Text/Blaze/Html/Radian.hs
+++ Text/Blaze/Html/Radian.hs
@@ -1,5 +1,31 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+
 module Text.Blaze.Html.Radian where
 
 import Text.Blaze
 import qualified Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as H hiding (form, label)
+import Text.Blaze.Internal
+
+
+plot :: H.Html -> H.Html
+plot = Parent "plot" "<plot" "</plot>"
+
+plotData :: H.Html -> H.Html
+plotData = Parent "plot-data" "<plot-data" "</plot-data>"
+
+plotOptions :: H.Html -> H.Html
+plotOptions = Parent "plot-options" "<plot-options" "</plot-options>"
+
+metadata :: H.Html 
+metadata = Leaf "metadata" "<metadata" ">"
+
+lines :: H.Html 
+lines = Leaf "lines" "<lines" ">"
+
+points :: H.Html 
+points = Leaf "points" "<points" ">"
+
+x = attribute "x" " x=\""
+
+y = attribute "y" " y=\""
