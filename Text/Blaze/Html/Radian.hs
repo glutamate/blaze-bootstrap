2d38a6f66b3a75b5b9cd90e192481dcbc952d450
Tom Nielsen <tanielsen@gmail.com>
2014-10-29 16:04:58 +0000

    cassava data for plots

--- Text/Blaze/Html/Radian.hs
+++ Text/Blaze/Html/Radian.hs
@@ -9,11 +9,16 @@ import qualified Text.Blaze.Html5.Attributes as H hiding (form, label)
 import Text.Blaze.Internal
 import Text.Blaze.Html.Bootstrap
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as DTE
+import Data.ByteString.Lazy (toStrict)
 import Data.String (fromString)
 import Data.Monoid
 import Prelude hiding (lines)
 import qualified Data.Array.Unboxed as Array
-import Data.List (foldl1')
+import Data.List (foldl1', intercalate)
+import qualified Data.Csv as CSV
+import qualified Data.Vector as V
+import OpenBrain.CSV
 
 plot :: H.Html -> H.Html
 plot = Parent "plot" "<plot" "</plot>"
@@ -30,16 +35,16 @@ plotCol = Parent "plot-col" "<plot-col" "</plot-col>"
 plotOptions :: H.Html -> H.Html
 plotOptions = Parent "plot-options" "<plot-options" "</plot-options>"
 
-metadata :: H.Html 
+metadata :: H.Html
 metadata = Parent "metadata" "<metadata" "</metadata>" $ ""
 
-lines :: H.Html 
+lines :: H.Html
 lines = Parent "lines" "<lines" "</lines>" $ ""
 
-bars :: H.Html 
+bars :: H.Html
 bars = Parent "bars" "<bars" "</bars>" $ ""
 
-points :: H.Html 
+points :: H.Html
 points = Parent "points" "<points" "</points>" $ ""
 
 
@@ -58,6 +63,17 @@ plotDataCSV nm hdrs rows
     = plotData ! H.name (H.toValue nm) ! cols (H.toValue $ T.intercalate "," hdrs) ! format "csv"
                $ preEscapedText $ "\n" <> (T.unlines $ map (T.intercalate ",") rows)
 
+cassavaPlotData :: CSV.ToNamedRecord a => T.Text -> [a] -> H.Html
+cassavaPlotData _ [] = return ()
+cassavaPlotData nm xs = do
+  let hdrs = allHeaders $ head xs
+      hdrsL = map (T.unpack . DTE.decodeUtf8) $ V.toList hdrs
+  let csv = toStrict $ CSV.encodeByName hdrs xs
+  plotData ! H.name (H.toValue nm) ! cols (H.toValue $ intercalate "," hdrsL) ! format "csv"
+     $ preEscapedText $ "\n" <> DTE.decodeUtf8 csv <> "\n"
+
+
+
 radianScripts :: H.Html
 radianScripts = do
   scriptLink "http://openbrainsrc.github.io/Radian/js/jquery.js"
@@ -69,24 +85,31 @@ radianScripts = do
   scriptLink "http://openbrainsrc.github.io/Radian/js/bootstrap.min.js"
 
 radianHeaders :: H.Html
-radianHeaders = do 
+radianHeaders = do
    H.meta ! H.charset "utf-8"
    styleSheetLink "http://openbrainsrc.github.io/Radian/css/radian.css"
 
 radianDummyModule :: T.Text -> H.Html
-radianDummyModule nm = 
-  H.script $ preEscapedText $ "angular.module('"<>nm<>"', ['radian']);" 
+radianDummyModule nm =
+  H.script $ preEscapedText $ "angular.module('"<>nm<>"', ['radian']);"
 
 ------------------
 -- ScatterPlot  --
 ------------------
 
-scatterPlot :: Int -> Int -> T.Text -> [(Double,Double)] -> H.Html
+scatterPlot, linePlot :: Int -> Int -> T.Text -> [(Double,Double)] -> H.Html
 scatterPlot h w nm xys = do
   let tshow = T.pack . show
   plotDataCSV nm ["x", "y"] $ map (\(x,y)-> [tshow x, tshow y]) xys
   plot  ! H.height (toValue h) ! H.width (toValue w) $ do
     points ! x (radExpr $ nm<>".x")
+           ! y (radExpr $ nm<>".y")
+
+linePlot h w nm xys = do
+  let tshow = T.pack . show
+  plotDataCSV nm ["x", "y"] $ map (\(x,y)-> [tshow x, tshow y]) xys
+  plot  ! H.height (toValue h) ! H.width (toValue w) $ do
+    lines  ! x (radExpr $ nm<>".x")
            ! y (radExpr $ nm<>".y")
 
 
@@ -100,13 +123,13 @@ dynHistogram :: T.Text -> [Double] -> H.Html
 dynHistogram nm xs = do
   plotDataCSV nm ["val"] $ map ((:[]) . T.pack . show) xs
   plot ! H.name (toValue $ "plot"<>nm) $ do
-     bars ! H.customAttribute "hist" (toValue $ "[[histogram("<> nm<>".val, 50)]]") 
-          ! x "[[hist.centres]]" 
+     bars ! H.customAttribute "hist" (toValue $ "[[histogram("<> nm<>".val, 50)]]")
+          ! x "[[hist.centres]]"
           ! y "[[hist.probs]]"
 
 exampleSines :: H.Html
-exampleSines = plot ! H.height "200" 
-                    ! aspect "2" 
+exampleSines = plot ! H.height "200"
+                    ! aspect "2"
                     ! strokeWidth "2"
                     ! x "[[seq(0,4*PI,101)]]" $ do
   lines ! y "[[sin(x)]]" ! stroke "red"
@@ -116,7 +139,7 @@ exampleSines = plot ! H.height "200"
 
 staticHistogram :: T.Text -> Int -> ((H.Html->H.Html) ->(H.Html->H.Html)) -> [Double] -> H.Html
 staticHistogram nm nbins modPlot xs = do
-  let hist = histValues nbins xs  
+  let hist = histValues nbins xs
       tshow = T.pack . show
   plotDataCSV nm ["val", "histcount"] $ map (\(x,y) -> [tshow x, tshow y]) hist
   (modPlot plot) ! H.name (toValue $ "plot"<>nm) $ do
@@ -127,7 +150,7 @@ histArr :: (Int, Int) -> [Int] -> Array.UArray Int Double
 histArr bnds is = Array.accumArray (+) 0 bnds [( i, 1) | i<-is, Array.inRange bnds i]
 
 histValues :: Int -> [Double] -> [(Double,Double)]
-histValues nbins vls = 
+histValues nbins vls =
     let (hArr, lo, hi, binSize) = histList nbins vls
     in zip [lo, lo+binSize..hi] hArr
 
@@ -141,4 +164,3 @@ histList nbins vls = let lo = foldl1' min vls
                          hArr = histArr (0,nbins-1) $ ixs
                          empiricalIntegral = sum $ map (*binSize) $ Array.elems hArr
                      in ((/empiricalIntegral) `fmap` Array.elems hArr, lo, hi, binSize)
-
