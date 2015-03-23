478bff38d98d56ef0171832d9e3d0300ff0234af
Tom Nielsen <tanielsen@gmail.com>
2015-03-23 13:15:40 +0000

    radian attributes

--- Text/Blaze/Html/Radian.hs
+++ Text/Blaze/Html/Radian.hs
@@ -59,9 +59,9 @@ points = Parent "points" "<points" "</points>" $ ""
 
 
 
-[x,y, cols, format, aspect, stroke, strokeWidth, axisXlabel, axisYlabel, marker, fill, markerSize] = map genAttribute $ words attrs where
+[x,y, cols, format, aspect, stroke, strokeWidth, axisXlabel, axisYlabel, marker, fill, markerSize, rangeX, xTicks, plotTitle, fillOpacity] = map genAttribute $ words attrs where
   genAttribute s = attribute (fromString s) (fromString (' ':s++"=\""))
-  attrs = "x y cols format aspect stroke stroke-width axis-x-label axis-y-label marker fill marker-size"
+  attrs = "x y cols format aspect stroke stroke-width axis-x-label axis-y-label marker fill marker-size range-x axis-x-ticks title fill-opacity"
 
 --x = attribute "x" " x=\""
 
